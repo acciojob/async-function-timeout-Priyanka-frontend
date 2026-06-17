781499ec644c840a8e6b8c810b49bf3d8d5535a3
Priyanka-frontend <kaushikpriyanka005@gmail.com>
2026-06-17 14:10:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+const textInput = document.getElementById("text");
+const delayInput = document.getElementById("delay");
+const button = document.getElementById("btn");
+const output = document.getElementById("output");
+
+function wait(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
+
+button.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const text = textInput.value;
+    const delay = Number(delayInput.value);
+
+    output.textContent = "";
+
+    await wait(delay);
+
+    output.textContent = text;
+});
